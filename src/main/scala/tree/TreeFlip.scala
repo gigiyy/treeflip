d28df359c32f8eccf59794bc46c4c40455289e7d
Guixin Zhu <guixin.zhu@yahoo.com>
2019-03-30 16:07:14 +0900

    build tree from position to node map

--- src/main/scala/tree/TreeFlip.scala
+++ src/main/scala/tree/TreeFlip.scala
@@ -1,5 +1,7 @@
 package tree
 
+import scala.collection.immutable.SortedSet
+
 object TreeFlip {
 
   def main(args: Array[String]): Unit = {
@@ -7,7 +9,37 @@ object TreeFlip {
   }
 
   def buildTree(map: Map[Pos, Node]): Node = {
-    ???
+    val ps = map.keySet
+    val rootRow = ps.filter(_.row == 0)
+    require(rootRow.size == 1, "there should be only on node on row 0")
+    val rootPos = rootRow.head
+    val root = map(rootPos)
+
+    def nodeOfRow(row: Int): SortedSet[Pos] = SortedSet.empty[Pos] ++ ps.filter(_.row == row)
+
+    var seem = Set(rootPos)
+    var curRow = 0
+    val cur = collection.mutable.Queue(rootPos)
+    var next = nodeOfRow(curRow + 1)
+    while (cur.nonEmpty) {
+      val parentPos = cur.dequeue
+
+      println(s"working on $parentPos")
+
+      val parent = map(parentPos)
+      val childrenPos = next.intersect(parentPos.possibleChildren).filter(!seem.contains(_))
+      childrenPos.foreach { p =>
+        seem += p
+        if (p == parentPos.leftChild) parent.left = Some(map(p))
+        else if (p == parentPos.rightChild) parent.right = Some(map(p))
+      }
+      if (cur.isEmpty) {
+        cur ++= next
+        curRow += 1
+        next = nodeOfRow(curRow + 1)
+      }
+    }
+    root
   }
 
   def parseInput(input: Seq[String]): Map[Pos, Node] =
@@ -23,8 +55,6 @@ object TreeFlip {
 
   def parseRow(ints: Array[Int]): Seq[(Int, Node)] =
     ints.zipWithIndex.filter(_._1 != 0).map(p => (p._2, Node(p._1)))
-
-
 }
 
 
@@ -36,4 +66,14 @@ object Node {
   def apply(id: Int) = new Node(id)
 }
 
-case class Pos(row: Int, col: Int)
+case class Pos(row: Int, col: Int) {
+  def possibleChildren: Set[Pos] = Set(leftChild, rightChild)
+
+  def leftChild = Pos(row + 1, col - 1)
+
+  def rightChild = Pos(row + 1, col + 1)
+}
+
+object Pos {
+  implicit val order: Ordering[Pos] = Ordering.by(p => (p.row, p.col))
+}
--- src/test/scala/tree/TreeFlipTest.scala
+++ src/test/scala/tree/TreeFlipTest.scala
@@ -10,14 +10,14 @@ class TreeFlipTest extends FunSuite with Matchers with
   test("array of ints to tree node") {
     assert(parseRow(Array(0, 0, 1, 0, 0)) === Seq((2, Node(1))))
   }
+  val input: Seq[String] = Seq(
+    "0, 0, 1, 0, 0",
+    "0, 2, 0, 3, 0",
+    "4, 0, 5, 0, 0",
+    "0, 6, 0, 7, 0"
+  ).map(s => s"[[$s]]")
 
   test("parse input to map") {
-    val input = Seq(
-      "0, 0, 1, 0, 0",
-      "0, 2, 0, 3, 0",
-      "4, 0, 5, 0, 0",
-      "0, 6, 0, 7, 0"
-    ).map(s => s"[[$s]]")
     val map = parseInput(input)
     assert(map === Map(
       Pos(0, 2) -> Node(1),
@@ -25,5 +25,17 @@ class TreeFlipTest extends FunSuite with Matchers with
       Pos(2, 0) -> Node(4), Pos(2, 2) -> Node(5),
       Pos(3, 1) -> Node(6), Pos(3, 3) -> Node(7)
     ))
+  }
+
+  test("build tree from map") {
+    val map = parseInput(input)
+    val root = buildTree(map)
+    assert(root.id === 1)
+    assert(root.left.map(_.id) === Some(2))
+    assert(root.right.map(_.id) === Some(3))
+    assert(root.left.flatMap(_.left.map(_.id)) === Some(4))
+    assert(root.left.flatMap(_.right.map(_.id)) === Some(5))
+    assert(root.left.flatMap(_.left.flatMap(_.right.map(_.id))) === Some(6))
+    assert(root.left.flatMap(_.right.flatMap(_.right.map(_.id))) === Some(7))
   }
 }
